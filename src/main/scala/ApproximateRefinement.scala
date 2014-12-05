1bd8588f1f416c531dab41c8553faa18e5d0f718
Jan Bessai <Jan.Bessai@tu-dortmund.de>
2014-12-05 07:21:37 +0100

    Simplified the example, throwing out generics

--- src/main/scala/ApproximateRefinement.scala
+++ src/main/scala/ApproximateRefinement.scala
@@ -6,13 +6,7 @@ object ApproximatedRefinementType {
     val tgt: Type
   }
 
-  type Arrow2[S1 <: Type, S2 <: Type, T <: Type] = Arrow {
-    val src: S1
-    val tgt: Arrow { val src: S2; val tgt: T }
-  }
-
-  type Source[A2 <: Arrow2[_ <: Type, _ <: Type, _ <: Type]] =
-    ({ val arrow: A2; type S = arrow.src.type })#S
+  type Source[A2 <: Arrow] = ({ val arrow: A2; type S = arrow.src.type })#S
 }
 
 
