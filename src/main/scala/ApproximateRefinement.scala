cc80b793ef9734a4da09270a85da0132be9984b4
Jan Bessai <Jan.Bessai@tu-dortmund.de>
2014-12-05 07:37:18 +0100

    Adds some testing

--- src/main/scala/ApproximateRefinement.scala
+++ src/main/scala/ApproximateRefinement.scala
@@ -7,6 +7,22 @@ object ApproximateRefinement {
   }
 
   type Source[A2 <: Arrow] = ({ val arrow: A2; type S = arrow.src.type })#S
+
+  // Testing it
+  case class Const(id: Int) extends Type
+  val sigma = Const(0)
+  val tau = Const(1)
+  val sigmaArrowTau = new Arrow {
+    val src: sigma.type = sigma
+    val tgt: tau.type = tau
+  }
+  def test(x: Arrow)(src: Source[x.type]): x.src.type = src
+  val sigmaCopy: sigma.type = test(sigmaArrowTau)(sigmaArrowTau.src)
+  val sigmaCopy2: sigma.type = test(sigmaArrowTau)(sigma)
+  val sigmaCopy3: sigma.type = test(sigmaArrowTau)(sigmaCopy)
+  val sigmaCopyInferred = test(sigmaArrowTau)(sigmaArrowTau.src)
+  val correctTypeInferenceTest = test(sigmaArrowTau)(sigmaCopyInferred)
+  val correctTypeInferenceTest2: sigma.type = test(sigmaArrowTau)(sigmaCopyInferred)
 }
 
 
